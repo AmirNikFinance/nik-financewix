495c3a6ae86ba3a15eb9616bef2fcc275fa21288
Wix Remote Machine <support@wix.com>
2026-01-28 03:59:17 +0000

    Set carousel to 5s

--- src/components/LendersCarousel.tsx
+++ src/components/LendersCarousel.tsx
@@ -182,7 +182,7 @@ export default function LendersCarousel() {
           className="flex gap-4 md:gap-6"
           animate={{ x: isHovered ? 0 : -100 }}
           transition={{
-            duration: isHovered ? 0 : 20,
+            duration: isHovered ? 0 : 5,
             ease: isHovered ? 'easeInOut' : 'linear',
             repeat: isHovered ? 0 : Infinity,
           }}

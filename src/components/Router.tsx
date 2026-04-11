ec7df458c6d0a5e985871c1ad0aaf14a51e58e56
Wix Remote Machine <support@wix.com>
2026-04-11 23:11:36 +0000

    Restoring previous version

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -10,14 +10,7 @@ import BlogPage from '@/components/pages/BlogPage';
 import BlogArticlePage from '@/components/pages/BlogArticlePage';
 import CalculatorsPage from '@/components/pages/CalculatorsPage';
 import CarLoanCalculatorPage from '@/components/pages/CarLoanCalculatorPage';
-import PartnerLoginPage from '@/components/pages/PartnerLoginPage';
-import PartnerPortalPage from '@/components/pages/PartnerPortalPage';
-import PartnerCommissionsPage from '@/components/partner/PartnerCommissionsPage';
-import PartnerReferralsPage from '@/components/partner/PartnerReferralsPage';
-import PartnerProfilePage from '@/components/partner/PartnerProfilePage';
-import PartnerSubmitReferralPage from '@/components/partner/PartnerSubmitReferralPage';
 import LendingAssistantPage from '@/components/pages/LendingAssistantPage';
-import { MemberProtectedRoute } from '@/components/ui/member-protected-route';
 import { initializeGoogleSheets } from '@/lib/googleSheets';
 import { GOOGLE_SHEETS_CONFIG } from '@/config/googleSheets.config';
 
@@ -68,50 +61,6 @@ const router = createBrowserRouter([
       {
         path: "car-loan-calculator",
         element: <CarLoanCalculatorPage />,
-      },
-      {
-        path: "partner-login",
-        element: <PartnerLoginPage />,
-      },
-      {
-        path: "partner-portal",
-        element: (
-          <MemberProtectedRoute messageToSignIn="Sign in to access the partner portal">
-            <PartnerPortalPage />
-          </MemberProtectedRoute>
-        ),
-      },
-      {
-        path: "partner-portal/commissions",
-        element: (
-          <MemberProtectedRoute messageToSignIn="Sign in to view commissions">
-            <PartnerCommissionsPage />
-          </MemberProtectedRoute>
-        ),
-      },
-      {
-        path: "partner-portal/referrals",
-        element: (
-          <MemberProtectedRoute messageToSignIn="Sign in to view referrals">
-            <PartnerReferralsPage />
-          </MemberProtectedRoute>
-        ),
-      },
-      {
-        path: "partner-portal/profile",
-        element: (
-          <MemberProtectedRoute messageToSignIn="Sign in to manage your profile">
-            <PartnerProfilePage />
-          </MemberProtectedRoute>
-        ),
-      },
-      {
-        path: "partner-portal/submit-referral",
-        element: (
-          <MemberProtectedRoute messageToSignIn="Sign in to submit a referral">
-            <PartnerSubmitReferralPage />
-          </MemberProtectedRoute>
-        ),
       },
       {
         path: "lending-assistant",
